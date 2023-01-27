4d6bc094fbcf4219eadc66b86fdbc3f01d1c97ab
Adam1322 <adam.kuliczkowski00@gmail.com>
2023-01-27 10:39:43 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Adam";
+bool kobieta = false;
+var age = 22;
+
+if (kobieta == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Ewa poniżej 30 lat");
+    }
+}  
+else if (age < 18 && kobieta == true)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+Console.WriteLine("Żadna z tych odpowiedźi");
