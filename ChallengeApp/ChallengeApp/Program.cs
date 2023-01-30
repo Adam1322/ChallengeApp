057a69e383b4ebe491a281f2127dadf12fd7d37d
Adam1322 <adam.kuliczkowski00@gmail.com>
2023-01-30 15:18:00 +0100

    Zadanie 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,43 @@
-﻿var name = "Adam";
-bool kobieta = false;
-var age = 22;
+﻿using ChallengeApp;
 
-if (kobieta == true)
+Employee employee0 = new Employee("Adam", "Kowalski", "30" );
+Employee employee1 = new Employee("Mariusz", "Szewczyk", "25");
+Employee employee2 = new Employee("Damian", "Krawczyk", "27");
+
+
+employee0.AddPoints(4);
+employee0.AddPoints(1);
+employee0.AddPoints(3);
+employee0.AddPoints(8);
+employee0.AddPoints(3);
+
+employee1.AddPoints(1);
+employee1.AddPoints(8);
+employee1.AddPoints(6);
+employee1.AddPoints(4);
+employee1.AddPoints(2);
+
+employee2.AddPoints(9);
+employee2.AddPoints(1);
+employee2.AddPoints(4);
+employee2.AddPoints(7);
+employee2.AddPoints(1);
+
+int maxPoints = -1;
+Employee employeeWithMaxResult = null;
+
+List<Employee> employees = new List<Employee>()
 {
-    if (name == "Ewa" && age == 33)
-    {
-        Console.WriteLine("Ewa lat 33");
-    }
-    else if (age < 30)
-    {
-        Console.WriteLine("Ewa poniżej 30 lat");
-    }
-}  
-else if (age < 18 && kobieta == true)
+    employee0, employee1, employee2
+};
+
+foreach (var employee in employees)
 {
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    if (employee.Points > maxPoints)
+    {
+        maxPoints = employee.Points;
+        employeeWithMaxResult = employee;
+    }
 }
-Console.WriteLine("Żadna z tych odpowiedźi");
+
+Console.WriteLine($" Najlepszy pracownik jest: {employeeWithMaxResult.name} {employeeWithMaxResult.surname}" + $" ma {employeeWithMaxResult.Points} punkty");
