db9e1af8d122889c4fa3495b680ea62400d38101
Adam1322 <adam.kuliczkowski00@gmail.com>
2023-02-15 11:40:04 +0100

    Zadanie 15

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,13 +1,18 @@
 ﻿namespace ChallengeApp
 {
-    public class Employee : Person
+    public class Employee : IEmployee
     {
         private List<float> grades = new List<float>();
 
         public Employee(string name, string surname, string age)
-            : base(name, surname, age)
         {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
         }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
 
         public void AddGrade(float grade)
         {
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,24 +1,18 @@
 ﻿using ChallengeApp;
 
-
 Console.WriteLine("Witamy w programie oceny Pracowników");
 Console.WriteLine("===========================================");
 Console.WriteLine();
-Console.WriteLine("Podaj imię");
-string name = Console.ReadLine();
-Console.WriteLine("Podaj nazwisko");
-string surname = Console.ReadLine();
-Console.WriteLine("Podaj wiek");
-string age = Console.ReadLine();
 
+Console.WriteLine("Podaj oceny pracownika i kierownika. Żeby zakończyć proces naciśnij 'q'");
+Console.WriteLine(" ");
 
+var supervisor = new Supervisor("Tomasz", "Bąk", "50");
 var employee = new Employee("Adam", "Kuliczkowski", "23");
 
 while (true)
 {
-    Console.WriteLine("Aby zakończyć ocenę wpisz: q ");
-    Console.WriteLine("Podaj kolejną ocenę");
-
+    Console.WriteLine("Podaj kolejną ocenę pracownika");
     var input = Console.ReadLine();
     if(input == "q")
     {
@@ -29,16 +23,37 @@ while (true)
     {
         employee.AddGrade(input);
     }
-    catch(Exception)
+    catch(Exception exception)
     {
-        Console.WriteLine($"Exception catched: {e.Message}");
+        Console.WriteLine($"Exception catched: {exception.Message}");
     }
 }
-var statistics = employee.GetStatistics();
+while (true)
+{
+    Console.WriteLine("Podaj kolejną ocenę kierownika");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+    try { supervisor.AddGrade(input); }
+    catch (Exception exception) { Console.WriteLine($"Exception catched:{exception.Message}"); }
+}
+var StatisticsEmployee = employee.GetStatistics();
+var StatisticsSupervior = supervisor.GetStatistics();
+
 Console.WriteLine();
+Console.WriteLine($"Pracownik {employee.Name} {employee.Surname} wiek : {employee.Age} lat");
 Console.WriteLine();
-Console.WriteLine($"{employee.Name} {employee.Surname} Lat {employee.Age}");
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Final {statistics.AverageLetter}");
+Console.WriteLine($"Ocena średnia: {StatisticsEmployee.Average:N2}");
+Console.WriteLine($"Ocena minimalna: {StatisticsEmployee.Min}");
+Console.WriteLine($"Ocena maksymalna: {StatisticsEmployee.Max}");
+Console.WriteLine($"Ocena końcowa: {StatisticsEmployee.AverageLetter}");
+
+Console.WriteLine();
+Console.WriteLine($"Kierownik {supervisor.Name} {supervisor.Surname} wiek : {supervisor.Age} lat");
+Console.WriteLine();
+Console.WriteLine($"Ocena średnia: {StatisticsSupervior.Average:N2}");
+Console.WriteLine($"Ocena minimalna: {StatisticsSupervior.Min}");
+Console.WriteLine($"Ocena maksymalna: {StatisticsSupervior.Max}");
+Console.WriteLine($"Ocena końcowa: {StatisticsSupervior.AverageLetter}");
