2a376012017d6c2f7f9f862e4f6725cf5fd6778d
Adam1322 <adam.kuliczkowski00@gmail.com>
2023-02-17 11:38:17 +0100

    Zadanie 16

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,53 +7,57 @@ Console.WriteLine();
 Console.WriteLine("Podaj oceny pracownika i kierownika. Żeby zakończyć proces naciśnij 'q'");
 Console.WriteLine(" ");
 
-var supervisor = new Supervisor("Tomasz", "Bąk", "50");
-var employee = new Employee("Adam", "Kuliczkowski", "23");
+var employee = new EmployeeInFile("Adam", "Kuliczkowski", "23");
+employee.AddGrade(0.5f);
+employee.AddGrade(13);
+employee.AddGrade(1);
+//var supervisor = new Supervisor("Tomasz", "Bąk", "50");
+//var employee = new Employee("Adam", "Kuliczkowski", "23");
 
-while (true)
-{
-    Console.WriteLine("Podaj kolejną ocenę pracownika");
-    var input = Console.ReadLine();
-    if(input == "q")
-    {
-        break;
-    }
+//while (true)
+//{
+//    Console.WriteLine("Podaj kolejną ocenę pracownika");
+//    var input = Console.ReadLine();
+//    if(input == "q")
+//    {
+//        break;
+//    }
 
-    try
-    {
-        employee.AddGrade(input);
-    }
-    catch(Exception exception)
-    {
-        Console.WriteLine($"Exception catched: {exception.Message}");
-    }
-}
-while (true)
-{
-    Console.WriteLine("Podaj kolejną ocenę kierownika");
-    var input = Console.ReadLine();
-    if (input == "q")
-    {
-        break;
-    }
-    try { supervisor.AddGrade(input); }
-    catch (Exception exception) { Console.WriteLine($"Exception catched:{exception.Message}"); }
-}
-var StatisticsEmployee = employee.GetStatistics();
-var StatisticsSupervior = supervisor.GetStatistics();
+//    try
+//    {
+//        employee.AddGrade(input);
+//    }
+//    catch(Exception exception)
+//    {
+//       Console.WriteLine($"Exception catched: {exception.Message}");
+//    }
+//}
+//while (true)
+//{
+//    Console.WriteLine("Podaj kolejną ocenę kierownika");
+//    var input = Console.ReadLine();
+//    if (input == "q")
+//    {
+//        break;
+//    }
+//    try { supervisor.AddGrade(input); }
+//    catch (Exception exception) { Console.WriteLine($"Exception catched:{exception.Message}"); }
+//}
+//var StatisticsEmployee = employee.GetStatistics();
+//var StatisticsSupervior = supervisor.GetStatistics();
 
-Console.WriteLine();
-Console.WriteLine($"Pracownik {employee.Name} {employee.Surname} wiek : {employee.Age} lat");
-Console.WriteLine();
-Console.WriteLine($"Ocena średnia: {StatisticsEmployee.Average:N2}");
-Console.WriteLine($"Ocena minimalna: {StatisticsEmployee.Min}");
-Console.WriteLine($"Ocena maksymalna: {StatisticsEmployee.Max}");
-Console.WriteLine($"Ocena końcowa: {StatisticsEmployee.AverageLetter}");
+//Console.WriteLine();
+//Console.WriteLine($"Pracownik {employee.Name} {employee.Surname} wiek : {employee.Age} lat");
+//Console.WriteLine();
+//Console.WriteLine($"Ocena średnia: {StatisticsEmployee.Average:N2}");
+//Console.WriteLine($"Ocena minimalna: {StatisticsEmployee.Min}");
+//Console.WriteLine($"Ocena maksymalna: {StatisticsEmployee.Max}");
+//Console.WriteLine($"Ocena końcowa: {StatisticsEmployee.AverageLetter}");
 
-Console.WriteLine();
-Console.WriteLine($"Kierownik {supervisor.Name} {supervisor.Surname} wiek : {supervisor.Age} lat");
-Console.WriteLine();
-Console.WriteLine($"Ocena średnia: {StatisticsSupervior.Average:N2}");
-Console.WriteLine($"Ocena minimalna: {StatisticsSupervior.Min}");
-Console.WriteLine($"Ocena maksymalna: {StatisticsSupervior.Max}");
-Console.WriteLine($"Ocena końcowa: {StatisticsSupervior.AverageLetter}");
+//Console.WriteLine();
+//Console.WriteLine($"Kierownik {supervisor.Name} {supervisor.Surname} wiek : {supervisor.Age} lat");
+//Console.WriteLine();
+//Console.WriteLine($"Ocena średnia: {StatisticsSupervior.Average:N2}");
+//Console.WriteLine($"Ocena minimalna: {StatisticsSupervior.Min}");
+//Console.WriteLine($"Ocena maksymalna: {StatisticsSupervior.Max}");
+//Console.WriteLine($"Ocena końcowa: {StatisticsSupervior.AverageLetter}");
